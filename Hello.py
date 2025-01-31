19b7716288d0821b1dbba22bf6f8934a294fd651
DiegoMejorada23 <dvmejorada@up.edu.ph>
2025-01-31 13:50:27 +0800

    initial commit added hello.py

---
+++ Hello.py
@@ -0,0 +1,11 @@
+print("Hello World")
+
+# git config --global --unset user.name
+# git config --global --unset user.email
+# git config --local user.name ****
+# git config --local user.email ****
+
+# git status
+
+# git add -A
+## add all files to stage
